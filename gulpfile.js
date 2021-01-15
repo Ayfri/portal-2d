e8a846eaf6936b241dd0582d364a8d10d081891d
Ayfri <pierre.ayfri@gmail.com>
2021-01-15 14:29:51 +0100

    Added gulpfile.js.

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,22 @@
-const gulp = require("gulp")
-const esbuild = require("gulp-esbuild")
+const gulp = require('gulp');
+const esbuild = require('gulp-esbuild');
 
 function bundle() {
 	return gulp
-		.src("./src/index.ts")
+		.src('./src/index.ts')
 		.pipe(
 			esbuild({
-				outfile: "bundle.js",
-				sourcemap: "inline",
+				outfile: 'bundle.js',
+				sourcemap: 'inline',
 				bundle: true,
-				target: ["chrome58", "firefox57", "safari11", "edge16"],
+				target: ['chrome58', 'firefox57', 'safari11', 'edge16'],
 				loader: {
-					".ts": "ts",
-					".json": "json",
+					'.ts': 'ts',
+					'.json': 'json',
 				},
 			})
 		)
-		.pipe(gulp.dest("./dist/"))
+		.pipe(gulp.dest('./dist/'));
 }
 
-exports.bundle = bundle
+exports.bundle = bundle;
