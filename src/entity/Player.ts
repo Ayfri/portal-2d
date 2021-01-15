87fedac4e49dc51e484ce80f0a43d526daa13674
Ayfri <pierre.ayfri@gmail.com>
2021-01-15 16:09:26 +0100

    Simplified player constructor.

--- src/entity/Player.ts
+++ src/entity/Player.ts
@@ -1,4 +1,13 @@
+import Entity from './Entity.js';
+
 export default class Player extends Entity {
-  public constructor() {
-     super();   }
+	public constructor() {
+		super('player');
+	}
+	
+	public load() {
+	}
+	
+	public update() {
+	}
 }
