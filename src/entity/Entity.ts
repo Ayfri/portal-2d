644af9a75ad0c6077ab183b815417cad47f5fb69
Ayfri <pierre.ayfri@gmail.com>
2021-01-15 16:09:01 +0100

    Added entity methods and it is now abstract.

--- src/entity/Entity.ts
+++ src/entity/Entity.ts
@@ -1,4 +1,23 @@
-export default class Entity  {
-  public constructor() {
-      }
+import * as PIXI from 'pixi.js';
+
+export default abstract class Entity extends PIXI.Sprite {
+	protected constructor(texture: string);
+	protected constructor(texture: PIXI.Texture);
+	protected constructor(texture: PIXI.Texture | string) {
+		if (typeof texture === 'string') texture = PIXI.Loader.shared.resources[texture].texture;
+		super(texture);
+		this.load();
+	}
+	
+	abstract load();
+	
+	abstract update();
+	
+	destroy(options?: {
+		children?: boolean;
+		texture?: boolean;
+		baseTexture?: boolean;
+	}) {
+		super.destroy(options);
+	}
 }
