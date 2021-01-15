d7a649e6180a7e59e9e349979f566bf0b519b2f8
Ayfri <pierre.ayfri@gmail.com>
2021-01-15 16:09:40 +0100

    Added player un ChamberScene.

--- src/scenes/ChamberScene.ts
+++ src/scenes/ChamberScene.ts
@@ -1,11 +1,16 @@
+import Player from '../entity/Player.js';
 import Scene from './Scene.js';
 
 export default class ChamberScene extends Scene {
+	public player: Player;
+	
 	public constructor() {
 		super();
+		this.player = new Player();
 	}
 	
 	public load() {
+		this.addChild(this.player);
 	}
 	
 	public update() {
