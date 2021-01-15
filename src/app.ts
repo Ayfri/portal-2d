a97caa007fb6c6cd44fac72d80111bc660789e23
Ayfri <pierre.ayfri@gmail.com>
2021-01-15 15:40:07 +0100

    Added first test sprite.

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,16 @@
 import * as PIXI from 'pixi.js';
 
 export const app = new PIXI.Application({
+	antialias: true,
 	resizeTo: window,
+});
+
+PIXI.Loader.shared.add('test', 'assets/test.png');
+PIXI.Loader.shared.load();
+PIXI.Loader.shared.onComplete.add(() => {
+	const sprite = PIXI.Sprite.from(PIXI.Loader.shared.resources['test'].texture);
+	sprite.position.set(window.innerWidth / 2, window.innerHeight / 2);
+	app.stage.addChild(sprite);
 });
 
 document.body.appendChild(app.view);
