79f32d452eb0648921e672a3cef53716620e2e88
Ayfri <pierre.ayfri@gmail.com>
2021-01-19 17:12:33 +0100

    Improved player and wall test configuration.

--- src/scenes/ChamberScene.ts
+++ src/scenes/ChamberScene.ts
@@ -11,13 +11,13 @@ export default class ChamberScene extends Scene {
 	public setup() {
 		this.player = new Player();
 		this.addChild(this.player);
-		this.player.position.set(window.innerWidth / 2, window.innerHeight / 2);
+		this.player.position.set(window.innerWidth / 2, window.innerHeight / 2 - 500);
 
 		this.addWall({
-			x: window.innerWidth / 2,
-			y: window.innerHeight / 2 + 100,
-			width: 50,
-			height: 50,
+			x: window.innerWidth / 2 - 300,
+			y: window.innerHeight / 2 - 100,
+			width: 300,
+			height: 400,
 		});
 	}
 }
