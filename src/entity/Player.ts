00e896c0b76e5697fcd6dd66275968ff0ffa7f55
Ayfri <pierre.ayfri@gmail.com>
2021-01-19 16:44:46 +0100

    Added movements for player.

--- src/entity/Player.ts
+++ src/entity/Player.ts
@@ -1,5 +1,6 @@
 import * as PIXI from 'pixi.js';
 import { actualScene } from '../app.js';
+import { D, Q } from '../client/InputManager.js';
 import * as mathUtils from '../utils/mathUtils';
 import Entity from './Entity';
 import WallEntity from './WallEntity';
@@ -9,26 +10,35 @@ export default class Player extends Entity {
 		super('player', {
 			hasGravity: true,
 		});
+
+		Q.on('down', () => this.move(new PIXI.Point(-1, this.velocity.y / 2), 2));
+		D.on('down', () => this.move(new PIXI.Point(1, this.velocity.y / 2), 2));
 	}
 
 	public setup() {}
 
-	public move(direction: PIXI.Point, speed: number) {
-		this.velocity = direction.set(direction.x * speed, direction.y * speed);
-	}
-
 	public update() {
 		const walls: WallEntity[] = actualScene.children.filter((entity: Entity): entity is WallEntity => entity instanceof WallEntity);
 
+		if (walls.every(wall => !mathUtils.isRectangleCollapse(this, wall))) this.canFall = true;
+
 		walls.forEach(wall => {
 			if (mathUtils.isRectangleCollapse(this, wall)) {
-				//				console.log(mathUtils.manageRectangleCollisions(this, wall));
-
-				let velocity: PIXI.Point = mathUtils.collisionResponse(this, wall);
-				this.velocity.set(velocity.x, -velocity.y);
+				const velocity: PIXI.Point = mathUtils.collisionResponse(this, wall);
+				this.position.x += velocity.x;
+				this.position.y += velocity.y;
 				if (velocity.y > 0) this.canFall = false;
+				console.log(velocity);
+				if (velocity.x > 0) {
+					this.velocity.x = 0;
+					this.position.x -= 1;
+				}
 			}
 		});
 		super.update();
+	}
+
+	public move(direction: PIXI.Point, speed: number): void {
+		this.velocity = direction.set(direction.x * speed, direction.y * speed);
 	}
 }
