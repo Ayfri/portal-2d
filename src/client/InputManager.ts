801ea4567b63a9bd43711069bd64783574f4a24b
Ayfri <pierre.ayfri@gmail.com>
2021-01-19 17:12:12 +0100

    Added keypress event for keys.

--- src/client/InputManager.ts
+++ src/client/InputManager.ts
@@ -1,27 +1,28 @@
 import * as PIXI from 'pixi.js';
 
 export const keys = new Set<Key>();
+export type KeyHandle = 'up' | 'down' | 'keypressed';
 
 class Key extends PIXI.utils.EventEmitter {
 	private _pressedAt: number = 0;
-
+	
 	constructor(public readonly id: string | number) {
 		super();
 		keys.add(this);
 	}
-
+	
 	private _isPressed: boolean = false;
-
+	
 	get isPressed(): boolean {
 		return this._isPressed;
 	}
-
+	
 	get duration(): number {
 		if (!this._isPressed) return 0;
 		return Date.now() - this._pressedAt;
 	}
-
-	handle(event: KeyboardEvent, action: 'up' | 'down') {
+	
+	handle(event: KeyboardEvent, action: KeyHandle) {
 		this.emit(action, event, this.duration);
 		if (action === 'down') {
 			this._isPressed = true;
@@ -30,17 +31,17 @@ class Key extends PIXI.utils.EventEmitter {
 			this._isPressed = false;
 		}
 	}
-
-	on(event: 'up' | 'down', fn: (event?: KeyboardEvent, duration?: number) => unknown, context?: any): this {
+	
+	on(event: KeyHandle, fn: (event?: KeyboardEvent, duration?: number) => unknown, context?: any): this {
 		super.on(event, fn, context);
 		return this;
 	}
-
-	once(event: 'up' | 'down', fn: (eventN: KeyboardEvent, durationN: number) => unknown, context?: any): this {
+	
+	once(event: KeyHandle, fn: (eventN: KeyboardEvent, durationN: number) => unknown, context?: any): this {
 		super.once(event, fn, context);
 		return this;
 	}
-
+	
 	isMineEvent(event: KeyboardEvent): boolean {
 		if (typeof this.id === 'string') return event.key === this.id;
 		return event.keyCode === this.id;
@@ -64,5 +65,11 @@ document.addEventListener('keydown', event => {
 document.addEventListener('keyup', event => {
 	keys.forEach(key => {
 		if (key.isMineEvent(event)) key.handle(event, 'up');
+	});
+});
+
+document.addEventListener('keypress', event => {
+	keys.forEach(key => {
+		if (key.isMineEvent(event)) key.handle(event, 'keypressed');
 	});
 });
