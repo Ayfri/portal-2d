91ca1d241ac5d93713e954ae54d6348461914125
Ayfri <pierre.ayfri@gmail.com>
2021-01-19 18:23:51 +0100

    Improved movement system in air & on ground.

--- src/entity/Player.ts
+++ src/entity/Player.ts
@@ -8,37 +8,47 @@ import WallEntity from './WallEntity';
 export default class Player extends Entity {
 	public constructor() {
 		super('player', {
-			hasGravity: true,
+			hasGravity: true
 		});
 	}
-
+	
 	public setup() {}
-
+	
 	public update() {
+		super.update();
 		const walls: WallEntity[] = actualScene.children.filter((entity: Entity): entity is WallEntity => entity instanceof WallEntity);
-
-		if (Q.isPressed && this.velocity.x > -5) this.move(new PIXI.Point(-1, 0), 0.4);
-		if (D.isPressed && this.velocity.x < 5) this.move(new PIXI.Point(1, 0), 0.4);
-
+		
+		if (this.isOnGround) {
+			if (Q.isPressed && this.velocity.x > -5) this.move(new PIXI.Point(-1, 0), 1.7);
+			if (D.isPressed && this.velocity.x < 5) this.move(new PIXI.Point(1, 0), 1.7);
+		} else {
+			if (Q.isPressed && this.velocity.x > -5) this.move(new PIXI.Point(-1, 0), 0.55);
+			if (D.isPressed && this.velocity.x < 5) this.move(new PIXI.Point(1, 0), 0.55);
+			this.velocity.x *= 0.92;
+		}
 		if (space.isPressed && !this.canFall) {
 			this.move(new PIXI.Point(0, -1), 2.5);
 			this.position.y += this.velocity.y;
 		}
-
-		if (walls.every(wall => !mathUtils.isRectangleCollapse(this, wall))) this.canFall = true;
-
+		
+		if (walls.every(wall => !mathUtils.isRectangleCollapse(this, wall))) {
+			this.canFall = true;
+			this.isOnGround = false;
+		}
+		
 		walls.forEach(wall => {
 			if (mathUtils.isRectangleCollapse(this, wall)) {
 				switch (mathUtils.manageRectangleCollisions(wall, this)) {
 					case 'bottom':
 						this.move(new PIXI.Point(this.velocity.x / 10, 1), 2);
 						break;
-
+					
 					case 'top':
 						this.canFall = false;
-						this.move(new PIXI.Point(-this.velocity.x / 20, 1), 2);
+						this.move(new PIXI.Point(-this.velocity.x / 5, 1), 2);
+						this.isOnGround = true;
 						break;
-
+					
 					case 'left':
 					case 'right':
 						this.move(new PIXI.Point(-this.velocity.x, 0), 1);
@@ -46,9 +56,8 @@ export default class Player extends Entity {
 				}
 			}
 		});
-		super.update();
 	}
-
+	
 	public move(direction: PIXI.Point, speed: number): void {
 		this.velocity.x += direction.x * speed;
 		this.velocity.y += direction.y * speed;
